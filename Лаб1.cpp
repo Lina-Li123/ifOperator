a75537351ac40f5cf368b1337e4bcfd7da803b89
Lina_Li <ilyinadother@gmail.com>
2020-07-13 23:54:23 +0300

    Реализация условного оператора на языке С++

---
+++ Лаб1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+void main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	cout << "Введите значение x";
+	int x;
+	const float a = 1;
+	cin >> x;
+	double y;
+	if (x < a)
+		y = (1 / tan(sqrt(abs(x)))) + pow(x, 2);
+	else
+		if (x > a)
+			y = log10(x * x + a * a);
+		else
+		{
+			y = a * powf(sin(3.14 * x), 2);  
+			cout << "3   y=" << y;
+		}
+	system("pause");
+}
